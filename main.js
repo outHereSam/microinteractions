84c0f3a2aa7ec84c5ac6325ff68c78e2748b0de7
Eric-Obeng <obengeric.3958@gmail.com>
2024-07-25 16:23:57 +0000

    form styles updated

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
 const form = document.getElementById("form");
 const input = document.getElementById("email");
 
+const thankYouEL = document.getElementById("thank-you");
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   console.log("submit");
+ 
   let email = input.value;
   if (!email) {
     input.classList.add("error");
-    e.preventDefault(); // Prevent form submission
+    e.preventDefault();
   } else {
-    input.classList.remove("error");
-    input.value = "";
+    form.classList.add("animate");
+    setTimeout(() => {
+      input.classList.remove("error");
+      input.value = "";
+      form.classList.add("hidden");
+      thankYouEL.classList.remove("hidden");
+      thankYouEL.classList.add("vissible");
+    }, 3000);
   }
 });
